4d02538adc7c998bb77e8e0704c8811e33502e15
KingaStefanska <kinga0stefanska@gmail.com>
2022-07-29 09:43:59 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const gallery = document.querySelector('div.gallery');
+const pictures = galleryItems
+  .map(element => {
+    const image = `<a class="gallery__item" href="${element.original}" >
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      alt="${element.description}"
+      title="${element.description}"
+    />
+  </a>`;
+    return image;
+  })
+  .join(' ');
+
+gallery.insertAdjacentHTML('afterbegin', pictures);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 console.log(galleryItems);
