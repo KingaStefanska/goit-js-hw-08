626771c58e1fe96d401ab4b9ddaf86dc47e3895c
KingaStefanska <kinga0stefanska@gmail.com>
2022-07-29 14:56:48 +0200

    update 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  }, 1000)
+);
+
+const actualTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(actualTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
